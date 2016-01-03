1e96ab618cb037e7a56ed1dc6ef82200a9dd2a39
Meher Ranjan <hmeherranjan@gmail.com>
2016-01-03 20:28:58 +0530

    perlin smoothe walker

--- js/perlinWalker.js
+++ js/perlinWalker.js
@@ -1,6 +1,8 @@
 var perlinWalker = {
     x: window.innerWidth/2,
     y: window.innerHeight/2,
+    z: 0,
+    t: 0,
     move: function(){
         var stepX = noise(this.z);
             stepX = map(stepX, 0, 1, 0, window.innerWidth);
@@ -9,8 +11,8 @@ var perlinWalker = {
         
         this.x = stepX;
         this.y = stepY;
-        this.z += 0.01;
-        this.t += 0.015;
+        this.z += 0.011;
+        this.t += 0.017;
     },
     map: function(value, istart, istop, ostart, ostop) {
       return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
@@ -20,14 +22,15 @@ var perlinWalker = {
     },
     display: function(c) {
         // c.clearRect(0, 0, window.innerWidth, window.innerHeight);
-        c.fillRect(this.x, this.y, 10, 10);
+        c.fillRect(this.x, this.y, 2, 2);
         c.fill();
         c.stroke();
     },
     update: function(c){
-        return false;
+        this.move();
+        this.display(c);
     },
     init: function(){
-        this.display(c);
+        return false;
     }
 }
